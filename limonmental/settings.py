a4442080aa0fa307542a353fd0b324db7602376c
Víctor Salgado <victorsalgado@MacBook-Air-de-Victor.local>
2023-11-19 12:34:10 -0300

    Change: ahora se usa mysql

--- limonmental/settings.py
+++ limonmental/settings.py
@@ -73,13 +73,16 @@ WSGI_APPLICATION = "limonmental.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'limonmental',
+            'USER': 'admin',
+            'PASSWORD': 'VfLz3Jd0',
+            'HOST': 'mysql-154867-0.cloudclusters.net',
+            'PORT': '12816',
+        }
     }
-}
 
 
 # Password validation
