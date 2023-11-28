b8874b3f526db4ee17378888e2537c7b711f950c
Víctor Salgado <victorsalgado@MacBook-Air-de-Victor.local>
2023-11-28 17:50:04 -0300

    Change

--- limonmental/settings.py
+++ limonmental/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-GS_CREDENTIALS_FILE = 'https://storage.googleapis.com/limonmental/psicologiaa-91e8a35121d0.json'
+GS_CREDENTIALS_FILE = os.path.join(BASE_DIR, 'credenciales.json')
 
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'limonmental' 
